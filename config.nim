eee8d51f83bd539fb3dfc5736c6ce99c94861edf
h3rald <h3rald@h3rald.com>
2021-03-15 19:37:59 +0100

    Updated to latest version of min.

--- config.nim
+++ config.nim
@@ -1,5 +1,5 @@
 const
   pkgName*        = "HastySite"
-  pkgVersion*     = "1.3.5"
+  pkgVersion*     = "1.3.6"
   pkgDescription* = "A small but powerful static site generator"
   pkgAuthor*      = "Fabio Cevasco"
